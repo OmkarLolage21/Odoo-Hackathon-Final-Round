e33aadab80ed98ed3d82c78eb0933e95c0fec61d
Nidhish Wakodikar <nidhishwakodikar@gmail.com>
2025-09-20 22:16:31 +0530

    added taxes logic and RABC

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -1,10 +1,21 @@
 from datetime import datetime
 from uuid import UUID
-from sqlalchemy import Enum, String, ForeignKey
+from sqlalchemy import Enum, String, ForeignKey, Numeric, Boolean
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 from sqlalchemy.sql import func
 
 from core.database import Base
+
+class Tax(Base):
+    __tablename__ = "taxes"
+    
+    id: Mapped[UUID] = mapped_column(primary_key=True, server_default=func.gen_random_uuid())
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    computation_method: Mapped[str] = mapped_column(Enum('percentage', 'fixed', name='computation_method_enum'), nullable=False)
+    value: Mapped[float] = mapped_column(Numeric(10, 2), nullable=False)
+    is_applicable_on_sales: Mapped[bool] = mapped_column(Boolean, server_default='true')
+    is_applicable_on_purchase: Mapped[bool] = mapped_column(Boolean, server_default='true')
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
 
 # Contact models
 
